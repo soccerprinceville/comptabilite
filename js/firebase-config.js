7850f4abfbc48dc8e82afc46780d506cae9f8cea
soccerprinceville <soccer.princeville@gmail.com>
2026-09-12 14:08:04 -0400

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 
 // URL du déploiement Apps Script (se termine par /exec).
 // Voir README.md, section "3. Déployer le serveur Google Apps Script".
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyR8mu6olPOU02e6w0IvseTGgbjz4V4jVOQn78EXOjFyasOM8hzVv9n-98G8DLH-rdW/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxA7ehAS2dNUGJFYC8wsykbS7uOEfaEj82Xrrp-v0zVAh37HUjiTEnEraoPkl_UrGO2/exec";
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
